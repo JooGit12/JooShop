1d2b8431dae6050b48747b34df19fa2fb0f08bdd
JooWon <joocode12@gmail.com>
2024-01-21 18:20:56 +0900

    주문 도메인 개발 주문 기능 test 작성

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -12,20 +12,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
     private final EntityManager em;
-
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+    }
     public void save(Order order) {
         em.persist(order);
     }
 
-    public Order findAllByString(Long id) {
+    public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
 
-    public List<Order> findAll(OrderSearch orderSearch) {
+    public List<Order> findAllByString(OrderSearch orderSearch) {
 
         String jpql = "select o from Order o join o.member m";
         boolean isFirstCondition = true;
